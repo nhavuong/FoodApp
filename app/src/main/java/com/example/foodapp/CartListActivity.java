c2547bd3f77395d27e93f7dc6b974bb3164a074e
nhavuong <nhatv@uci.edu>
2021-03-08 19:47:16 -0800

    add

--- app/src/main/java/com/example/foodapp/CartListActivity.java
+++ app/src/main/java/com/example/foodapp/CartListActivity.java
@@ -4,25 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.foodapp.adapter.CartAdapter;
 import com.example.foodapp.model.Cart;
 import com.example.foodapp.model.Food;
-import com.example.foodapp.retrofit.ApiInterface;
-import com.example.foodapp.retrofit.RetrofitClient;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
 
+import org.json.JSONException;
+
+import java.math.BigDecimal;
 import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class CartListActivity extends AppCompatActivity {
 
@@ -49,13 +51,13 @@ public class CartListActivity extends AppCompatActivity {
 
 
 
-        btn_order.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(v.getContext() ,PaymentActivity.class);
-                startActivity(i);
-            }
-        });
+//        btn_order.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(v.getContext() , PaymentsActivity.class);
+//                startActivity(i);
+//            }
+//        });
 
     }
 
@@ -80,6 +82,54 @@ public class CartListActivity extends AppCompatActivity {
         recycle_cart.setLayoutManager(layoutManager);
         recycle_cart.setAdapter(cartAdapter);
 
+    }
+
+    private static PayPalConfiguration config = new PayPalConfiguration()
+            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
+            .clientId("AZzIokUiGGTxL9RVse9ipFywdDNS4ux9h_d1RcddAsS6uf9qFdko3Vn8ut7GKsnVkMXKBUcuPd9xziA5");
+
+    public void beginPayment(View view){
+        Intent serviceConfig = new Intent(this, PayPalService.class);
+        serviceConfig.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+        startService(serviceConfig);
+
+        System.out.println("Total = " + total);
+        PayPalPayment payment = new PayPalPayment(new BigDecimal(String.valueOf(1 + total)), "USD", "Total:", PayPalPayment.PAYMENT_INTENT_SALE);
+
+        Intent paymentConfig = new Intent(this, PaymentActivity.class);
+        paymentConfig.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config); //send the same configuration for restart resiliency
+        paymentConfig.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
+        startActivityForResult(paymentConfig, 0);
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            PaymentConfirmation confirm = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+            if (confirm != null) {
+                try {
+                    Log.i("sampleapp", confirm.toJSONObject().toString(4));
+
+                    // TODO: send 'confirm' to your server for verification.
+                    // see https://developer.paypal.com/webapps/developer/docs/integration/mobile/verify-mobile-payment/
+                    // for more details.
+
+                } catch (JSONException e) {
+                    Log.e("sampleapp", "an extremely unlikely failure occurred: ", e);
+                }
+            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            Log.i("sampleapp", "The user canceled.");
+        } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
+            Log.i("sampleapp", "An invalid Payment or PayPalConfiguration was submitted. Please see the docs.");
+        }
+    }
+
+    @Override
+    public void onDestroy(){
+        stopService(new Intent(this, PayPalService.class));
+        super.onDestroy();
     }
 
 
--- app/src/main/java/com/example/foodapp/Config/Config.java
+++
@@ -1,6 +0,0 @@
-package com.example.foodapp.Config;
-
-public class Config {
-    public static final String PAYPAL_CLIENT_ID = "AZzIokUiGGTxL9RVse9ipFywdDNS4ux9h_d1RcddAsS6uf9qFdko3Vn8ut7GKsnVkMXKBUcuPd9xziA5";
-
-}
